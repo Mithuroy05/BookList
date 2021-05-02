41c1b7507aa99d52611d69ba888c74165cb6ba78
MITHU ROY <77204692+Mithuroy05@users.noreply.github.com>
2021-05-02 18:24:20 +0600

    Add files via upload

---
+++ Atray  BL 6.0..1.c
@@ -0,0 +1,92 @@
+/*Computer Semester Book Lest*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int deperment;
+    printf("Enter your Deperment Code=  ");
+    scanf("%d",&deperment);
+    if(deperment==66)
+    {
+        int semester;
+     printf("                ....]<<Computer Semester Book Lest>>[....             ");
+    printf("\n\nEnter your semester=  ");
+    scanf("%d",&semester);
+    if(semester<=1)
+        printf("\n(1)1011=Engineeing Dawing\n(2)5712=Enlish-1\n(3)5911=Mathematics-1\n(4)5913=Chemisty\n(5)6611=computer Fundamenal\n(6)6711=Basic Electricity\n(7)6811=Basic Electronic");
+    else if(semester<=2)
+        printf("\n(1)5712=English-2\n(2)5812=Physical Education & Life Skill Development\n(3)5912=Physics-1\n(4)5921=Mathematics-2\n(5)6621=Computer Applicaion-1\n(6)5711=Bangka\n(7)6821=Electronic Device & Circuis-1\n(8)7011=Basic Workshop Practice");
+    else if(semester<=3)
+        printf("\n(1)6622=Programming Language-1\n(2)6632=Computer Application-2\n(3)6841=Digital Electronics-1\n(4)6832=Industrial Electronics\n(5)5931=Mathematics-3\n(6)5922=Physics-2\n(7)5811=Social science-1");
+    else if(semester<=4)
+        printf("\n(1)6642=Programming Language-2\n(2)6645=CAD & Graphic Design\n(3)6844=Digital Electronics-2\n(4)6631=Data Stuctue & Algoithm\n(5)6745=Electrial Circuits & Machines\n(6)5942=Discrete Mathmatics\n(7)5821=Social Scienc-2");
+    else if(semester<=5)
+        printf("\n(1)6652=Micropocessor And Microcomputer-1\n(2)6652=Computer Architecture\n(3)6653=Programming Language-3\n(4)6654=Database Managent System\n(5)8551=Web Design\n(6)5851=Book Keeping & Accounting\n(7)5840=Environmental Managment");
+    else if(semester<=6)
+        printf("\n(1)6661=Micropocessor And Microcomputer-2\n(2)6662=Computer Peripherals\n(3)6663=Data Communication And Computer Netwok-1\n(4)6664=Computer System Software\n(5)6665=Computer Servicing\n(6)8563=Web Development\n(7)5841=Business Oganization & Communication");
+    else if(semester<=7)
+        printf("\n(1)6671=Embedded Suystem & PLC\n(2)8572=Multimudia And Graphics\n(3)6672=System Analysis And Design\n(4)6673=Data Communication And Computer Netwok-2\n(5)6674=Computer Engineering Project\n(6)5852=Industrial Management\n(7)5853=Eepeneurship");
+    else if(semester<=8)
+        printf("\nIndustrial Taining");
+    else
+        printf("\nOhh!!......Sorry!!!!!!!!\nPlease)> Enter your semester.\n\nDipeloma in Computer Engineer Has 8 Semester.\nSo try to 1-8 semester. ");
+        printf("\n\n\nOk good by........\nThank you for your request.");
+    }
+    else if(deperment==64)
+    {
+        int semester;
+     printf("                ....]<<Civil Semester Book Lest>>[....             ");
+    printf("\n\nEnter your semester=  ");
+    scanf("%d",&semester);
+    if(semester<=1)
+        printf("\n(1)1011=Engineeing Dawing\n(2)5712=Enlish-1\n(3)5911=Mathematics-1\n(4)5711=Bangla\n(5)5912=Physics-1\n(6)6711=Basic Electricity\n(7)7011=Basic Workshop Practice");
+    else if(semester<=2)
+        printf("\n(1)5712=English-2\n(2)1012Engineering Materials\n(3)5912=Physics-2\n(4)5921=Mathematics-2\n(5)6621=Computer Applicaion-1\n(6)5811=Social Science-1\n(7)6811=Basic Electronic \n");
+    else if(semester<=3)
+        printf("\n(1)6431=Civil Engineering Drawing(CAD)-1\n(2)6432=Surveying-1\n(3)6433=Structural Mechanics\n(4)5913=Chymistry\n(5)5931=Mathematics-3\n(6)5821=Social Science-2\n(7)5812=Physical Education & Life Skill Development");
+    else if(semester<=4)
+        printf("\n(1)6632=Computer Application-2\n(2)6443=Surveying-2\n(3)6442=Estimating & Costing-1\n(4)6441=Geotechnical Engineering\n(5)6444=Constuction Process-1\n(6)6445=Hydraulic\n(7)5841=Business Oganization & Communication");
+    else if(semester<=5)
+        printf("\n(1)6451=Fundamenal Engineering\n(2)6452=Surveying-3\n(3)6453=Environmental Engieering-1\n(4)6454=Theory Of Structure\n(5)6455=Constuction Process-2\n(6)5851=Book Keeping & Accounting\n(7)5840=Environmental Managment");
+    else if(semester<=6)
+        printf("\n(1)6464=Advance Surveying\n(2)6462=Transportation Engineeing\n(3)6463=Design Of Structure-1\n(4)6461=Civil Engineering Drawing(CAD)-2\n(5)6465=Construction Management\n(6)6659=Programming in C\n(7)5852=Industrial Management");
+    else if(semester<=7)
+        printf("\n(1)6471=Estimating & Costing-2\n(2)6472=Environmental Engieering-2\n(3)6473=Transportation Engineeing-2\n(4)6474=Design Of Structure-2\n(5)6475=Hydrology & Water Resources Engineering\n(6)6476=Civil Engineering Project\n(7)5853=Eepeneurship");
+    else if(semester<=8)
+        printf("\nIndustrial Taining");
+    else
+        printf("\nOhh!!......Sorry!!!!!!!!\nPlease)> Enter your semester.\n\nDipeloma in Civil Engineer Has 8 Semester.\nSo try to 1-8 semester. ");
+        printf("\n\n\nOk good by........\nThank you for your request.");
+    }
+    else if(deperment==67)
+    {
+           int semester;
+     printf("                ....]<<Electrical Semester Book Lest>>[....             ");
+    printf("\n\nEnter your semester=  ");
+    scanf("%d",&semester);
+    if(semester<=1)
+        printf("\n(1)1011=Engineeing Drawing\n(2)5712=Enlish-1\n(3)5911=Mathematics-1\n(4)5913=Chemisty\n(5)1012=Engineering Meterials\n(6)6711=Basic Electricity\n(7)5812=Physical Education & Life Skill Development");
+    else if(semester<=2)
+        printf("\n(1)5712=English-2\n(2)6721=Electrical Circuit-1\n(3)5912=Physics-1\n(4)5921=Mathematics-2\n(5)6621=Computer Applicaion-1\n(6)6722=Advanced Electricity\n(7)7011=Basic Workshop Practice");
+    else if(semester<=3)
+        printf("\n(1)6731=Electrical Circuit-2\n(2)6632=Computer Application-2\n(3)6811=Basic Electronic\n(4)5711=Bangla\n(5)5931=Mathematics-3\n(6)5922=Physics-2\n(7)5811=Social science-1");
+    else if(semester<=4)
+        printf("\n(1)6741=Electrical Appliances\n(2)6742=Electrical Installation Planning & Estimating\n(3)6845=Advanced Electronics-2\n(4)5941=Applied Mathematics\n(5)6743=Electrial Engineeing Drawing\n(6)5841=Business Oganization & Communication\n(7)5821=Social Scienc-2");
+    else if(semester<=5)
+        printf("\n(1)6751=Generation Of Electrical Energy\n(2)6752=DC Machine\n(3)6753=Electrical & Electronic Measurements-1\n(4)6855=Industrial Power Electronic\n(5)7043=Applied Mechanics\n(6)5851=Book Keeping & Accounting\n(7)7064=Hydraulics & Hydraulics Machinery");
+    else if(semester<=6)
+        printf("\n(1)6761=AC Machine-1\n(2)6762=Electrical & Electronic Measurements-2\n(3)6763=Transmission And Distribution Of Electrical Enery-1\n(4)6866=Digital Electronic & Microprocessors\n(5)5852=Industrial Management\n(6)5840=Environmental Management");
+    else if(semester<=7)
+        printf("\n(1)6771=AC Machine-2\n(2)6772=Electrical Engineering Project\n(3)6773=Switch Gear & Protection\n(4)6774=Transmission And Distribution Of Electrical Enery-2\n(5)6877=Instrumntation And Process Control\n(6)6659=Computer Progamming Language\n(7)5853=Eepeneurship");
+    else if(semester<=8)
+        printf("\nIndustrial Taining");
+    else
+        printf("\nOhh!!......Sorry!!!!!!!!\nPlease)> Enter your semester.\n\nDipeloma in Electrical Engineer Has 8 Semester.\nSo try to 1-8 semester. ");
+        printf("\n\n\nOk good by........\nThank you for your request.");
+    }
+     else
+        printf("\n\nOhh!!!!!.......Sorry\n\nWrong Deperment");
+        printf("\n\n\n\<........................SD Mithu Roy..............................>");
+
+    getch();
+}
